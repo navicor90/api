bcb3d2c8b913a3500061497b228a4d6503aec5f9
Franco Canizo <lfncanizo@gmail.com>
2015-09-02 17:43:48 -0300

    Se agregan docstrings para documentar y correr pruebas sobre los metodos validadores

--- app/mod_profiles/validators/globalValidator.py
+++ app/mod_profiles/validators/globalValidator.py
@@ -1,22 +1,50 @@
 # -*- coding: utf-8 -*-
 
 def is_int(var):
+    """Valida que el parámetro recibido sea un número entero
+    
+    >>> is_int(42)
+    42
+
+    >>> is_int('hola')
+    Traceback (most recent call last):
+        ...
+    ValueError: El valor ingresado no es un entero válido
+    """
     if not isinstance(var, int):
         raise ValueError("El valor ingresado no es un entero válido")
     else:
         return var
 
 def positive_int(var):
+    """Valida que un entero sea positivo
+
+    >>> positive_int(4)
+    4
+
+    >>> positive_int(-4)
+    Traceback (most recent call last):
+        ...
+    ValueError: El valor ingresado debe ser un entero positivo
+
+    >>> positive_int('hola')
+    Traceback (most recent call last):
+        ...
+    ValueError: El valor ingresado no es un entero válido
+    """
     if is_int(var) <= 0:
         raise ValueError("El valor ingresado debe ser un entero positivo")
     else:
         return var
-    """
-    elif var <= 0:
-        raise ValueError("El valor ingresado debe ser un entero positivo")
-    """
 
 def has_int(string):
+    """Determina si una cadena de caracteres contiene un número
+    >>> has_int('hola')
+    False
+
+    >>> has_int('ho12la')
+    True
+    """
     for c in string:
         try:
             int(c)
@@ -27,22 +55,16 @@ def has_int(string):
 
 
 def string_without_int(var):
+    """Valida que una cadena de caracteres no tenga números
+    >>> string_without_int('hola')
+    'hola'
+
+    >>> string_without_int('ho12la')
+    Traceback (most recent call last):
+        ...
+    ValueError: El texto no puede contener enteros
+    """
     if has_int(var):
         raise ValueError("El texto no puede contener enteros")
     else:
         return var
-        
-
-
-if __name__ == "__main__":
-    num_pos = 23
-    num_neg = -1
-    string = "hola"
-    string_num = "ho12la"
-    #print is_int(num_pos)
-    #print is_int(string)
-    print positive_int(num_pos)
-    #print positive_int(num_neg)
-    #print positive_int(string)
-    print string_without_int(string)
-    print string_without_int(string_num)
