3c6500a70af9167135565a74a22ac2073ae2a36d
adamantike <adamantike@users.noreply.github.com>
2015-10-07 22:55:13 -0300

    Eliminación de modelo, persistencia y recursos relacionados a Epicrisis

--- app/mod_profiles/adapters/__init__.py
+++ app/mod_profiles/adapters/__init__.py
@@ -2,4 +2,6 @@
 
 from .dropboxAdapter import DropboxAdapter
 from .yesDocAdapter import YesDocAdapter
+
+# Se importa último, ya que hace uso del resto de adaptadores.
 from .fileManagerFactory import FileManagerFactory
