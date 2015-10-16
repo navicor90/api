bab8d0ab68ef335da2e57e576aac7f2949985695
adamantike <adamantike@users.noreply.github.com>
2015-10-16 17:20:20 -0300

    Inclusión de validación del dueño del análisis en recursos

--- app/mod_profiles/resources/lists/analysisAnalysisFileList.py
+++ app/mod_profiles/resources/lists/analysisAnalysisFileList.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from flask import g
 from flask_restful import Resource, marshal_with
 from flask_restful_swagger import swagger
 
+from app.mod_shared.models.auth import auth
 from app.mod_profiles.models import Analysis
 from app.mod_profiles.common.fields.analysisFileFields import AnalysisFileFields
-from app.mod_profiles.common.swagger.responses.generic_responses import code_200_found, code_404
+from app.mod_profiles.common.swagger.responses.generic_responses import code_200_ok, code_403, code_404
 
 
 class AnalysisAnalysisFileList(Resource):
@@ -29,14 +31,21 @@ class AnalysisAnalysisFileList(Resource):
             },
         ],
         responseMessages=[
-            code_200_found,
+            code_200_ok,
+            code_403,
             code_404
         ]
     )
+    @auth.login_required
     @marshal_with(resource_fields, envelope='resource')
     def get(self, analysis_id):
         # Obtiene el análisis.
         analysis = Analysis.query.get_or_404(analysis_id)
+
+        # Verifica que el usuario autenticado sea el dueño del análisis
+        # especificado.
+        if g.user.id != analysis.profile.user.id:
+            return '', 403
 
         # Obtiene todos los archivos de análisis asociados al análisis.
         analysis_files = analysis.analysis_files.all()
--- app/mod_profiles/resources/lists/analysisMeasurementList.py
+++ app/mod_profiles/resources/lists/analysisMeasurementList.py
@@ -1,11 +1,13 @@
 # -*- coding: utf-8 -*-
 
+from flask import g
 from flask_restful import Resource, marshal_with
 from flask_restful_swagger import swagger
 
+from app.mod_shared.models.auth import auth
 from app.mod_profiles.models import Analysis
 from app.mod_profiles.common.fields.measurementFields import MeasurementFields
-from app.mod_profiles.common.swagger.responses.generic_responses import code_200_found, code_404
+from app.mod_profiles.common.swagger.responses.generic_responses import code_200_ok, code_403, code_404
 
 
 class AnalysisMeasurementList(Resource):
@@ -29,14 +31,21 @@ class AnalysisMeasurementList(Resource):
             },
         ],
         responseMessages=[
-            code_200_found,
+            code_200_ok,
+            code_403,
             code_404
         ]
     )
+    @auth.login_required
     @marshal_with(resource_fields, envelope='resource')
     def get(self, analysis_id):
         # Obtiene el análisis.
         analysis = Analysis.query.get_or_404(analysis_id)
+
+        # Verifica que el usuario autenticado sea el dueño del análisis
+        # especificado.
+        if g.user.id != analysis.profile.user.id:
+            return '', 403
 
         # Obtiene todas las mediciones asociadas al análisis.
         measurements = analysis.measurements.all()
