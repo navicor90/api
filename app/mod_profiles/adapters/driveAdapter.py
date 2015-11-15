07622acb85836098adbb83a907a71d012aaac752
Franco Canizo <lfncanizo@gmail.com>
2015-11-14 22:19:15 -0300

    Se agregan comentarios

--- app/mod_profiles/adapters/driveAdapter.py
+++ app/mod_profiles/adapters/driveAdapter.py
@@ -6,20 +6,28 @@ from googleapiclient import errors
 from googleapiclient.http import MediaIoBaseUpload
 from googleapiclient.discovery import build
 import httplib2
-import json
+
 
 
 class DriveAdapter(object):
     def __init__(self, token):
-        self.token = "ya29.KgJ2Kxr-ObdVR1qVrC-vNThgM0Yg6qIL1TGUaeQA6PMpFvW6IrgdwYIx3UF9kEhZX2gt"
+        self.token = token
 
     def get_service(self):
+        """
+        :return: servicio de drive
+        """
         credentials = AccessTokenCredentials(self.token, None)
         http = httplib2.Http()
         http = credentials.authorize(http)
         return build('drive', 'v2', http)
 
     def get_folder_id(self, folder_name, drive_service):
+        """
+        :param folder_name: nombre de la carpeta
+        :param drive_service: servicio de drive
+        :return: id de la carpeta en drive
+        """
         param = {}
         param['q'] = "title = '{0}' and mimeType = 'application/vnd.google-apps.folder'".formta(folder_name)
         try:
@@ -31,6 +39,12 @@ class DriveAdapter(object):
         return folder_id
 
     def get_file_id(self, file_name, folder_id, drive_service):
+        """
+        :param file_name: nombre del archivo
+        :param folder_id: id de la carpeta en drive
+        :param drive_service: servicio de drive
+        :return: id del archivo en drive
+        """
         param = {}
         param['q'] = "title = '{0}'".format(file_name)
         try:
@@ -42,15 +56,21 @@ class DriveAdapter(object):
         return file_id
 
     def upload_file(self, img_file):
+        """
+        Carga un archivo en la cuenta de drive
+        :param img_file: archivo
+        :return: diccionario que indica la ruta del archivo, descripción y la ubicación de
+        almacenamiento
+        """
         folder_name = 'YesDoc'
         filename = secure_filename(img_file.filename)
         drive_service = self.get_service()
         try:
-            # Busco la carpeta YesDoc en el directorio raíz
+            # Obtengo el id de la carpeta YesDoc en drive
             folder_id = self.get_folder_id(folder_name, drive_service)
-            # Compruebo que me devuelva un id lo que indica si existe
+            # Compruebo que me devuelva un id para la carpeta
+            # Si no la encuentra, creo la carpeta YesDoc en el directorio raíz
             if folder_id == None:
-                # Si no está, creo la carpeta YesDoc en el directorio raíz
                 body = {
                     'title': 'YesDoc',
                     'description': 'Carpeta de YesDoc: Asistente médico personal.',
@@ -58,7 +78,6 @@ class DriveAdapter(object):
                 }
                 yesdoc_folder = drive_service.files().insert(body=body).execute()
                 folder_id = yesdoc_folder['id']
-            # Cargo el archivo a la carpeta YesDoc
             media = MediaIoBaseUpload(img_file, chunksize=1024 * 1024, mimetype='image/png', resumable=True)
             body = {
                 'title': filename,
@@ -69,25 +88,35 @@ class DriveAdapter(object):
                 }],
                 'mimeType': 'image/png'
             }
-            response_file = drive_service.files().insert(media_body=media, body=body).execute()
+            # Cargo el archivo a la carpeta YesDoc
+            drive_service.files().insert(media_body=media, body=body).execute()
         except errors.HttpError, error:
             print 'Error en la carga a drive: %s' % error
             return None
+        # Armo la respuesta para con los datos para AnalysisFile
         res = {
             'path': '/'+folder_name+'/'+filename,
-            'description': 'Carga del archivo {0} en Dropbox'.format(folder_name),
+            'description': 'Carga del archivo {0} en Drive'.format(folder_name),
             'storage_location': 'Dirve'
         }
         return res
 
     def download_file(self, path):
+        """
+        Descarga un archivo
+        :param path: ruta del archivo en drive
+        :return: archivo
+        """
         list = path.split('/')
+        # Obtengo de la ruta el nombre de la carpeta que contiene el archivo
         folder_name = list[len(list)-2]
+        # Obtengo de la ruta el nombre del archivo
         file_name = list[len(list)-1]
         drive_service = self.get_service()
         folder_id = self.get_folder_id(folder_name, drive_service)
         file_id = self.get_file_id(file_name, folder_id, drive_service)
         try:
+            # Descargo la imágen de drive
             img_file = drive_service.files().get_media(fileId=file_id).execute()
         except errors.HttpError, error:
             print 'Error en la carga a drive: %s' % error
@@ -95,6 +124,11 @@ class DriveAdapter(object):
         return img_file
 
     def delete_file(self, path):
+        """
+        Elimina el archivo
+        :param path: ruta del archivo en drive
+        :return: nombre del archivo
+        """
         list = path.split('/')
         folder_name = list[len(list)-2]
         file_name = list[len(list)-1]
@@ -111,6 +145,11 @@ class DriveAdapter(object):
 
 
     def get_thumbnail(self, path):
+        """
+        Devuelve la vista previa de un archivo
+        :param path: ruta del archivo en drive
+        :return: imágen de vista previa
+        """
         list = path.split('/')
         folder_name = list[len(list)-2]
         file_name = list[len(list)-1]
